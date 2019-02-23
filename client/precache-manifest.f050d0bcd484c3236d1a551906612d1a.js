1cda96514872b3fa2da9d2e423ae388753b5ede0
Muhammad Aslam Abdurrohim <aslam.abdurrohim@tokopedia.com>
2019-02-23 11:57:02 +0700

    first commit

---
+++ client/precache-manifest.f050d0bcd484c3236d1a551906612d1a.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = [
+  {
+    "revision": "f7c32f438e01ff9caaea",
+    "url": "/static/css/main.5773e62e.chunk.css"
+  },
+  {
+    "revision": "f7c32f438e01ff9caaea",
+    "url": "/static/js/main.f7c32f43.chunk.js"
+  },
+  {
+    "revision": "3da0eb2b132979ad3fb8",
+    "url": "/static/css/1.b01ac8a9.chunk.css"
+  },
+  {
+    "revision": "3da0eb2b132979ad3fb8",
+    "url": "/static/js/1.3da0eb2b.chunk.js"
+  },
+  {
+    "revision": "229c360febb4351a89df",
+    "url": "/static/js/runtime~main.229c360f.js"
+  },
+  {
+    "revision": "8c8b5ec1800f9b50b837b7e18be72bc6",
+    "url": "/static/media/placeholder-broken.8c8b5ec1.jpeg"
+  },
+  {
+    "revision": "2a0d40ff78893721108724247287a2c3",
+    "url": "/static/media/about-photo.2a0d40ff.jpg"
+  },
+  {
+    "revision": "2dab70b008850e75732402db5620f13c",
+    "url": "/static/media/background.2dab70b0.jpg"
+  },
+  {
+    "revision": "865eccb63cc24b9ec8325bae7afcef92",
+    "url": "/index.html"
+  }
+];
